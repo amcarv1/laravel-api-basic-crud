d88d31eb867889d50b2cfc7bf0daee369c36ddf9
amcarv1 <erickamorimcarv@gmail.com>
2022-06-24 22:53:40 -0300

    products table controller created and api routes too. crud of products completed

--- my-laravel-project/routes/api.php
+++ my-laravel-project/routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ProductController;
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('produtos', [ProductController::class, 'listProduct']);
+Route::post('produtos', [ProductController::class, 'storeProduct']);
+Route::put('produtos/{id}/editar', [ProductController::class, 'updateProduct']);
+Route::delete('produtos/{id}', [ProductController::class, 'removeProduct']);
